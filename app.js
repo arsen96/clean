5e69c31d0b9c420b0ac2f0956274d1c07de1beda
Arsen Kubatyan <kubatarsen@gmail.com>
2019-05-02 14:38:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.render('contact');
+})
+
+app.post('/send', (req,res) => {
+    const output = `
+    <h2>Vous avez un nouveau message</h3>
+    <ul>
+     <li>Nom: ${req.body.nom}</li>
+     <li>Prénom: ${req.body.prenom}</li>
+     <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    ${req.body.message}
+    `
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'kubatarsen@gmail.com',
+            pass: 'arsenkubat'
+        }
+    })
+
+
+    let info =  transporter.sendMail({
+        from: `${req.body.email}`, // sender address
+        to: "kubatarsen@gmail.com", // list of receivers
+        subject: "...", // Subject line
+        text: `${req.body.message}`, // plain text body
+        html: output // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    res.render('contact', {msg: 'Le message a bien été envoyé'})
+}
+
+    )
+
+app.listen(3000, () => console.log('server started'));
